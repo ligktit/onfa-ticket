6be2953d68382ac124b1121231bcbce53d994ed0
Nguyễn Khoa Tuyên <ligktit@gmail.com>
2026-01-17 13:52:56 +0700

    Update RegistrationForm.jsx

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -705,8 +705,9 @@ const RegistrationForm = ({ onSuccess }) => {
                         <Copy className="w-5 h-5 text-gray-600" />
                       )}
                     </button>
-                    <p className="text-sm sm:text-base text-gray-600 font-medium">ONFA Chain</p>
                   </div>
+                  <div className="flex items-center gap-2"><p className="text-sm sm:text-base text-gray-600 font-medium text-center">ONFA Chain</p></div>
+                  
                 </div>
               </div>
             </div>
